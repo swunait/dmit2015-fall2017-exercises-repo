c492fbc569afc0bbe5004f3c956087a3798352f6
Sam Wu <swu@nait.ca>
2017-10-05 15:48:01 -0600

    Added example on how to retrieve a list of related entities

--- dmit2015-chinook-exercises-swu/src/main/java/chinook/controller/TrackController.java
+++ dmit2015-chinook-exercises-swu/src/main/java/chinook/controller/TrackController.java
@@ -4,9 +4,13 @@ import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.enterprise.inject.Model;
+import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 
+import org.omnifaces.util.Messages;
+
 import chinook.data.TrackRepository;
+import chinook.model.Genre;
 import chinook.model.Track;
 
 @Model
@@ -25,5 +29,55 @@ public class TrackController {
 	public List<Track> getTracks() {
 		return tracks;
 	}
+	
+	private List<Track> tracksByGenre;	// getter
+	private int currentSelectedGenreId;	// getter/setter
+//	private Genre currentSelectedGenre;	// getter
+	
+	public void findTracksByGenre() {
+		if( !FacesContext.getCurrentInstance().isPostback() ) {
+			// verify that a valid genreId was set
+			if( currentSelectedGenreId > 0) {
+				tracksByGenre = trackRepository.findAllByGenreId(currentSelectedGenreId);
+				if( tracksByGenre.size() == 0 ) {
+					Messages.addGlobalInfo("There are no tracks for genreID {0}", 
+							currentSelectedGenreId);
+				}
+			} else {
+				Messages.addGlobalError("Bad request. A valid genreID is required.");
+			}
+		}
+	}
+
+	public int getCurrentSelectedGenreId() {
+		return currentSelectedGenreId;
+	}
+
+	public void setCurrentSelectedGenreId(int currentSelectedGenreId) {
+		this.currentSelectedGenreId = currentSelectedGenreId;
+	}
+
+	public List<Track> getTracksByGenre() {
+		return tracksByGenre;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 }
--- dmit2015-chinook-exercises-swu/src/main/java/chinook/data/TrackRepository.java
+++ dmit2015-chinook-exercises-swu/src/main/java/chinook/data/TrackRepository.java
@@ -1,5 +1,7 @@
 package chinook.data;
 
+import java.util.List;
+
 import chinook.model.Track;
 
 public class TrackRepository extends AbstractJpaRepository<Track> {
@@ -8,5 +10,27 @@ public class TrackRepository extends AbstractJpaRepository<Track> {
 	public TrackRepository() {
 		super(Track.class);
 	}
+	
+	public List<Track> findAllByGenreId(int genreId) {
+		return getEntityManager().createQuery(
+			"SELECT t FROM Track t WHERE t.genre.genreId = :idValue", Track.class)
+			.setParameter("idValue", genreId)
+			.getResultList();
+	}
 
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
