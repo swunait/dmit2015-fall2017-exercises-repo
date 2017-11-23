f6b31f6d5504b37ac2e7fe4bbaa08c2a556b552b
Sam Wu <swu@nait.ca>
2017-11-23 13:49:27 -0700

    Forgot to commit InvoiceService.

--- dmit2015-chinook-exercises-swu/src/main/java/chinook/service/InvoiceService.java
+++ dmit2015-chinook-exercises-swu/src/main/java/chinook/service/InvoiceService.java
@@ -81,45 +81,5 @@ public class InvoiceService {
 		
 		return invoiceId;
 	}
-	
-	public int createInvoice(Invoice newInvoice, List<InvoiceLine> items) 
-			throws NoInvoiceLinesException, IllegalQuantityException {		
-		int invoiceId = 0;
-		
-		if (items == null || items.size() == 0) {
-			context.setRollbackOnly();
-			throw new NoInvoiceLinesException("There are no items in the invoice");			
-		}
-		
-		// assign the invoiceDate and total
-		Date today = Calendar.getInstance().getTime();
-		newInvoice.setInvoiceDate( new Timestamp(today.getTime()) );
-		// calculate the invoice total
-		double total = 0;
-		for(InvoiceLine singleItem : items) {
-			total += singleItem.getQuantity() * singleItem.getUnitPrice().doubleValue();
-		}
-		// set the invoice total
-		newInvoice.setTotal(BigDecimal.valueOf(total));
-		// persist the invoice
-		entityManager.persist(newInvoice);
-		// get the system generated invoiceId 
-		invoiceId = newInvoice.getInvoiceId();
-		
-		// iterate through each InvoiceLine and persist it
-		for (InvoiceLine singleItem : items) {
-			// rollback the transaction if quantity is less than one
-			if (singleItem.getQuantity() < 1) {
-				context.setRollbackOnly();
-				throw new IllegalQuantityException("Invalid quantity ordered.");
-			}
-			// set the invoice of each InvoiceLine
-			singleItem.setInvoice(newInvoice);
-			// persist the InvoiceLine
-			entityManager.persist(singleItem);
-		}		
-		
-		return invoiceId;
-	}
 
 }
